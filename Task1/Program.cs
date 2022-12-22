a803f0dc8325f7261fac7f499d20c672481599ec
smira12 <m1ronsa89@gmail.com>
2022-12-22 11:23:04 +0300

    Add Task №41

---
+++ Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// /*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+// */
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите элементы через пробел: ");
+int[] array = Array.ConvertAll((Console.ReadLine() ?? "").Split(), int.Parse);
+PrintArray(array);
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Кол-во элементов > 0: {count}");
